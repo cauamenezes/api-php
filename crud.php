93e40a2e35359985dfa7838a3cbd31158b35f8c1
cauamenezes <cauamenezes.07@gmail.com>
2021-11-05 10:59:24 -0300

    crud feito

--- crud.php
+++ crud.php
@@ -3,7 +3,7 @@
     ##IMPORTAÇÃO DO ARQUIVO DE CONEXÃO
     include('connection.php');
 
-    ## FUNÇÃO DE LEITURA DE DADOS (SEM CRITÉRIO) ##
+    ## FUNÇÃO DE LEITURA DE DADOS (LÊ TODOS OS DADOS) ##
     function read($conn){
         $sql = "SELECT * FROM tbl_pessoa";
 
@@ -19,6 +19,23 @@
         
     }
 
+     ## FUNÇÃO DE LEITURA DE DADOS (LÊ OS DADOS DE UMA PESSOA EM ESPECÍFICO) ##
+     function readID($cod_pessoa, $conn) {
+
+        $sql = "SELECT * FROM tbl_pessoa WHERE cod_pessoa = $cod_pessoa";
+
+        if ($resultado = mysqli_query($conn, $sql)) {
+
+            $dados = mysqli_fetch_all($resultado);
+
+            echo json_encode(array("status"=>"success", "data"=>$dados));
+            
+        } else {
+            echo json_encode(array("status"=>"error", "data"=>"Não há dados referentes a esse ID"));
+        }
+
+     }
+
     ## FUNÇÃO DE INSERÇÃO ##
     function create($nome, $sobrenome, $email, $celular, $fotografia, $conn) {
         
@@ -28,6 +45,36 @@
             echo json_encode(array("status"=>"success", "data"=>"Dados inseridos com sucesso"));
         } else {
             echo json_encode(array("status"=>"error", "data"=>"Erro ao inserir os dados"));
+        }
+    }
+
+     ## FUNÇÃO DE ATUALIZAÇÃO ##
+     function update($cod_pessoa, $nome, $sobrenome, $email, $celular, $fotografia, $conn) {
+        
+        $sql = "UPDATE tbl_pessoa SET
+        nome = '$nome',
+        sobrenome = '$sobrenome',
+        email = '$email',
+        celular = '$celular',
+        fotografia = '$fotografia'
+        WHERE cod_pessoa = $cod_pessoa";
+
+        if (mysqli_query($conn, $sql)) {
+            echo json_encode(array("status"=>"success", "data"=>"Dados alterados com sucesso"));
+        } else {
+            echo json_encode(array("status"=>"error", "data"=>mysqli_error($conn)));
+        }
+    }
+
+    ## FUNÇÃO DE EXCLUSÃO ##
+    function delete($cod_pessoa, $conn) {
+
+        $sql = "DELETE FROM tbl_pessoa WHERE cod_pessoa = $cod_pessoa";
+
+        if (mysqli_query($conn, $sql)) {
+            echo json_encode(array("status"=>"success", "data"=>"Dados excluidos com sucesso"));
+        } else {
+            echo json_encode(array("status"=>"error", "data"=>mysqli_error($conn)));
         }
     }
 
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+//dá acesso ao POSTMAN através do Apache
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: *");
     header("Access-Control-Allow-Methods: GET, POST");
@@ -18,6 +19,11 @@
         read($conn);
     }
 
+    if($acao == "readID") {
+        $cod_pessoa = $_REQUEST["cod_pessoa"];
+        readID($cod_pessoa ,$conn);
+    }
+
     ## ROTA DO CREATE ##
     if($acao == "create") {
         $nome = $_REQUEST["nome"];
@@ -27,6 +33,25 @@
         $fotografia = $_REQUEST["fotografia"];
 
         create($nome, $sobrenome, $email, $celular, $fotografia, $conn);
+    }
+
+    ## ROTA DO UPDATE ##
+    if($acao == "update") {
+        $cod_pessoa = $_REQUEST["cod_pessoa"];
+        $nome = $_REQUEST["nome"];
+        $sobrenome = $_REQUEST["sobrenome"];
+        $email = $_REQUEST["email"];
+        $celular = $_REQUEST["celular"];
+        $fotografia = $_REQUEST["fotografia"];
+
+        update($cod_pessoa, $nome, $sobrenome, $email, $celular, $fotografia, $conn);
+    }
+
+    ## ROTA DO DELETE ##
+    if ($acao == "delete") {
+
+        $cod_pessoa = $_REQUEST["cod_pessoa"];
+        delete($cod_pessoa, $conn);
     }
 
 ?>
